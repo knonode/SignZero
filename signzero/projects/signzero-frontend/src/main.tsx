bcfb8e2323a7c4f421d92a7c2df304ee6f2e57a9
knonode <algohmp@gmail.com>
2026-02-14 21:35:13 +0100

    removed Kibisis and Exodus from UI

--- signzero/projects/signzero-frontend/src/main.tsx
+++ signzero/projects/signzero-frontend/src/main.tsx
@@ -1,45 +1,34 @@
-import { Buffer } from 'buffer'
-globalThis.Buffer = Buffer
+import { Buffer } from "buffer";
+globalThis.Buffer = Buffer;
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { WalletProvider } from '@txnlab/use-wallet-react'
-import {
-  WalletManager,
-  WalletId,
-  NetworkId,
-} from '@txnlab/use-wallet'
-import { ThemeProvider } from './ThemeContext'
-import { ToastProvider } from './components/Toast'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { WalletProvider } from "@txnlab/use-wallet-react";
+import { WalletManager, WalletId, NetworkId } from "@txnlab/use-wallet";
+import { ThemeProvider } from "./ThemeContext";
+import { ToastProvider } from "./components/Toast";
+import "./index.css";
+import App from "./App";
 
-const networkId = (import.meta.env.VITE_NETWORK || 'localnet') as string
+const networkId = (import.meta.env.VITE_NETWORK || "localnet") as string;
 
 const getNetworkId = (): NetworkId => {
   switch (networkId) {
-    case 'testnet':
-      return NetworkId.TESTNET
-    case 'mainnet':
-      return NetworkId.MAINNET
+    case "testnet":
+      return NetworkId.TESTNET;
+    case "mainnet":
+      return NetworkId.MAINNET;
     default:
-      return NetworkId.LOCALNET
+      return NetworkId.LOCALNET;
   }
-}
+};
 
 const walletManager = new WalletManager({
-  wallets: [
-    WalletId.DEFLY,
-    WalletId.PERA,
-    WalletId.LUTE,
-    WalletId.KIBISIS,
-    WalletId.EXODUS,
-    WalletId.KMD,
-  ],
+  wallets: [WalletId.DEFLY, WalletId.PERA, WalletId.LUTE, WalletId.KMD],
   defaultNetwork: getNetworkId(),
-})
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <ToastProvider>
@@ -48,5 +37,5 @@ createRoot(document.getElementById('root')!).render(
         </WalletProvider>
       </ToastProvider>
     </ThemeProvider>
-  </StrictMode>
-)
+  </StrictMode>,
+);
